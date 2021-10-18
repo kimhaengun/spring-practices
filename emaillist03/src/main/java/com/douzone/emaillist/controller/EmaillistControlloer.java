aea1a0b8c4b9778e129c3824d84520bce4aca51f
kimhaengun <chxl89@naver.com>
2021-10-18 15:50:35 +0900

    emaillist03 @RequestMapping 적용

--- emaillist03/src/main/java/com/douzone/emaillist/controller/EmaillistControlloer.java
+++ emaillist03/src/main/java/com/douzone/emaillist/controller/EmaillistControlloer.java
@@ -1,11 +1,16 @@
 package com.douzone.emaillist.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.douzone.emaillist.repository.EmaillistRepository;
+import com.douzone.emaillist.vo.EmaillistVo;
 
 @Controller
 public class EmaillistControlloer {
@@ -13,10 +18,25 @@ public class EmaillistControlloer {
 	@Autowired
 	private EmaillistRepository emaillstRepository;
 	
-	@ResponseBody
 	@RequestMapping("")
-	public String index() {
-		System.out.println(emaillstRepository);
-		return "Emaillist Index";
+	public String index(Model model) {
+		List<EmaillistVo> list = emaillstRepository.findAll();
+		model.addAttribute("list",list);
+		
+		return "/WEB-INF/views/index.jsp";
 	}
+	
+	@RequestMapping(value = "/form",method = RequestMethod.GET)
+	public String form() {
+		return "/WEB-INF/views/form.jsp";
+	}
+	
+	@RequestMapping(value = "/add",method = RequestMethod.POST)
+	public String add(EmaillistVo vo) {
+		System.out.println(vo);
+		emaillstRepository.insert(vo);
+		
+		return "redirect:/";
+	}
+	
 }
