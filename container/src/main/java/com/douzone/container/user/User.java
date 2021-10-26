414661e45fb507c73505ecf42c54ef02e0f2fcac
kimhaengun <chxl89@naver.com>
2021-10-26 14:27:35 +0900

    wiring #1

---
+++ container/src/main/java/com/douzone/container/user/User.java
@@ -0,0 +1,6 @@
+package com.douzone.container.user;
+
+public class User {
+	private Long no = 0L;
+	private String name = "므므";
+}
---
+++ container/src/main/java/com/douzone/container/user/test/XmlConfigTest.java
@@ -0,0 +1,11 @@
+package com.douzone.container.user.test;
+
+public class XmlConfigTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
