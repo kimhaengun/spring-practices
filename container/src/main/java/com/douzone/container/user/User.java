5f1db1de9323c59f112bf1c36b21a6564309f316
kimhaengun <chxl89@naver.com>
2021-10-26 15:24:47 +0900

    Bean 생성 자동,명시적 #1

--- container/src/main/java/com/douzone/container/user/User.java
+++ container/src/main/java/com/douzone/container/user/User.java
@@ -3,4 +3,26 @@ package com.douzone.container.user;
 public class User {
 	private Long no = 0L;
 	private String name = "므므";
+	
+	public User() {
+		
+	}
+	public User(String name) {
+		this.name = name;
+	}
+	public User(Long no,String name) {
+		this.no = no;
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "User [no=" + no + ", name=" + name + "]";
+	}
+	public void setNo(Long no) {
+		this.no = no;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
