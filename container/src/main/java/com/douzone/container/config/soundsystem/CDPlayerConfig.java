ca9c4f5468c3a973303d34ddbe16b536e27c726b
kimhaengun <chxl89@naver.com>
2021-10-27 11:59:01 +0900

    java config

---
+++ container/src/main/java/com/douzone/container/config/soundsystem/CDPlayerConfig.java
@@ -0,0 +1,11 @@
+package com.douzone.container.config.soundsystem;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//스캔할 패키지
+@ComponentScan(basePackages = {"com.dounzone.container.soundsystem"})
+public class CDPlayerConfig {
+
+}
