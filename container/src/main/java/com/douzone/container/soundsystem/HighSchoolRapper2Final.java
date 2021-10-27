fa905a697e021a40c5d8c13962cd10d1461336f5
kimhaengun <chxl89@naver.com>
2021-10-27 11:23:03 +0900

    junit 사용

---
+++ container/src/main/java/com/douzone/container/soundsystem/HighSchoolRapper2Final.java
@@ -0,0 +1,17 @@
+package com.douzone.container.soundsystem;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HighSchoolRapper2Final implements CompactDisct {
+	
+	private String artist = "김하온";
+	private String title = "붕붕";
+	
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+		System.out.println("Playing "+title+" by "+artist);
+	}
+
+}
--- container/src/test/java/com/douzone/container/config/soundsystem/CDPlayerXmlConfigTest.java
+++ container/src/test/java/com/douzone/container/config/soundsystem/CDPlayerXmlConfigTest.java
@@ -3,30 +3,52 @@ package com.douzone.container.config.soundsystem;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.douzone.container.soundsystem.CDPlayer;
+import com.douzone.container.soundsystem.CompactDisct;
 
 //Test Container 생성
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:com/douzone/container/config/soundsystem/CDPlayerConfig.xml")
 public class CDPlayerXmlConfigTest {
-	
+	@Rule
+	public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
 	@Autowired
 	private CDPlayer cdplayer;
-	
-	@Test
-	public void testCDPlayerNotNull() {
-		assertNotNull(cdplayer);
-	}
+	@Autowired
+	private CompactDisct cd;
 	
 	@Test
 	public void testHello() {
 		int a= 10 - 9;
 		assertEquals(1, a); // a의 결과가 1이여야 성공
 	}
+	
+	@Test
+	public void testCDPlayerNotNull() {
+		//notnull 체크
+		assertNotNull(cdplayer);
+	}
+	
+	@Test
+	public void testCompactDiscNotNull() {
+		//notnull 체크
+		assertNotNull(cd);
+	}
+	
+//	@Ignore <<- 테스트에서 제외시킬 수 있다
+	@Test
+	public void testPlay() {
+		cdplayer.play();
+		assertEquals("Playing 붕붕 by 김하온",systemOutRule.getLog().replace("\r\n", ""));
+	}
+	
 }
